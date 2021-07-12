d158b17a821f21ad580da313996ad171b05ed65d
alangak <alanga@live.com>
2021-07-12 05:32:46 +0530

    Initial Code Setup

---
+++ server.js
@@ -0,0 +1,20 @@
+import express, { json, urlencoded } from "express";
+
+const app = express();
+
+// parse requests of content-type - application/json
+app.use(json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Node sample application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
